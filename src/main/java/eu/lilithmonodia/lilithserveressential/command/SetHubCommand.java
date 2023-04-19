59aafc4fcc449d45b750f3fbc50ea0f315fb8bdd
Pierre Camplin <pierre.camplin.etu@univ-lille.fr>
2023-04-19 13:08:10 +0200

    adding a way to test

--- src/main/java/eu/lilithmonodia/lilithserveressential/command/SetHubCommand.java
+++ src/main/java/eu/lilithmonodia/lilithserveressential/command/SetHubCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -22,7 +23,14 @@ public class SetHubCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        plugin.getConfig().set("lobby-world", args[0]);
+        if (args.length >= 1) {
+            plugin.getConfig().set("lobby-world", args[0]);
+        } else if (sender instanceof Player player) {
+            plugin.getConfig().set("lobby-world", player.getWorld());
+        } else {
+            sender.sendMessage("Please enter a valid argument");
+            return false;
+        }
         plugin.saveConfig();
         plugin.reloadConfig();
         sender.sendMessage(plugin.getConfiguration().lobbyWorld());
