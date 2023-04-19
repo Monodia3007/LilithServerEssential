99fb546f8432b3d0ee1b0b94e375f1acbaffa560
Pierre Camplin <pierre.camplin.etu@univ-lille.fr>
2023-04-19 12:44:06 +0200

    Testing new command

---
+++ src/main/java/eu/lilithmonodia/lilithserveressential/command/SetHubCommand.java
@@ -0,0 +1,37 @@
+package eu.lilithmonodia.lilithserveressential.command;
+
+import eu.lilithmonodia.lilithserveressential.LilithServerEssential;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SetHubCommand implements CommandExecutor, TabCompleter {
+    LilithServerEssential plugin;
+
+    public SetHubCommand (LilithServerEssential plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        plugin.getConfig().addDefault("lobby-world", label);
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        List<String> worlds = new ArrayList<>();
+        for (World world:Bukkit.getWorlds()) {
+            worlds.add(world.getName());
+        }
+        return worlds;
+    }
+}
